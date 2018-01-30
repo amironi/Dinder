da09e3dacd8d1e405fcaa0f1830e043bca33c165
amir <am5050@gmail.com>
2018-01-30 19:40:30 +0200

    m

--- src/components/EventsList.js
+++ src/components/EventsList.js
@@ -27,52 +27,51 @@ class EventsList extends Component {
     componentDidMount() {
       loadInitialEvents( (events) => this.setState({events}))
     }
-
   
     onEventSeleced(index = null){
+      index && this.setState({ prevEvents:  Object.assign({},  this.state.events)} )
+
       this.setState({currectID = index } );
     }
+
+    save(){
+      updateEvent(
+        events[currectID]),
+        () => {},
+        () => this.setState({ error: ('נא למלא  מקומות לאירוח*')  } )
+    }
+
+    cancel(){
+      this.setState({ events: this.state.prevEvents} )
+    }
+
     newEvent(){
       this.onEventSeleced( 
         this.state.events.push({ food: require('../data') }) -1);
     }
 
-    cancel(){
-      this.onEventSeleced();
-
-      //TODO remove this and restore prev stat
-      this.componentDidMount();
+    back(){
+      this.setState({currectID = null } );
     }
-
-    saveChanges(){
-      updateEvent(
-        events[currectID]),
-        () => this.setState({currectID = null } ),
-        () => this.setState({ error: ('נא למלא  מקומות לאירוח*')  } )
-    }
-
-
-
 
     render() {
       
-      const {currectID,events,error} = this.state;
+      const {currectID,events,error,prevEvents} = this.state;
 
       return (
         <View style={styles.container}>
 
-          {<Button 
-              transparent
-              style={{color: "#fff"}}
-              title={currectID ? 'שמור' : 'חדש'}
-              onPress={() => currectID ? this.saveChanges() : this.newEvent()}/>}
+        {!currectID && <Button transparent style={{color: "#fff"}}
+              title={'חדש'}
+              onPress={() => this.newEvent()}/>}
+        
+        {currectID && <Button transparent style={{color: "#fff"}}
+              title={prevEvents === events ? 'Save' : 'Cancel'}
+              onPress={() => prevEvents === events ? this.save() : this.cancel()}/>}
   
-          {currectID && <Button 
-              transparent
-              style={{color: "#fff"}}
-              title={'Back'}
-              onPress={() =>  this.cancel()}/>}
-
+          {currectID && <Button transparent style={{color: "#fff"}}
+              title={'<Back'}
+              onPress={() => this.back()}/>}
 
           {currectID === null &&<FlatList 
             data={events}
@@ -80,7 +79,6 @@ class EventsList extends Component {
               event={item} 
               onPressed={() => this.onEventSeleced(index) } /> } }
               ListFooterComponent={<View style={styles.footer}></View>} /> }}
-
           
           {currectID && <EventDetails error={error} event={events[currectID]} /> }
 
